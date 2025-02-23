c0c9b0b33ef98716e66f2f0d86c583859f993e15
Bala Logesh S <balassmp@gmail.com>
2025-02-23 16:35:19 -0500

    Added scripts to test apis and seed db

---
+++ test.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Script to test the API Endpoints
+
+METHOD=$1
+ID=$2
+
+URL="http://localhost:3000/account"
+
+if [ $METHOD == "help" ]; then
+    echo "./test.sh <METHOD> <ID?>"
+fi
+
+# Get all contacts
+if [[ $METHOD == 'get' && -z $ID ]]; then
+    curl -X GET $URL
+fi
+
+# Get contact with ID
+if [[ $METHOD == 'get' && -n $ID ]]; then
+    if [ -z $ID ]; then 
+        echo "ID is requried"
+        exit 1
+    fi
+
+    curl -X GET $URL/$ID
+fi
+
+# Create a new contact
+if [ $METHOD == 'post' ]; then
+    curl -X POST $URL \
+        -H "Content-Type: application/json" \
+        -d '{
+            "firstName": "John",
+            "lastName": "Doe"
+        }'
+
+    curl -X POST $URL \
+        -H "Content-Type: application/json" \
+        -d '{
+            "firstName": "Jane",
+            "lastName": "Hill"
+        }'
+fi
+
+# Delete a contact
+if [ $METHOD == 'delete' ]; then
+    if [ -z $ID ]; then 
+        echo "ID is requried"
+        exit 1
+    fi
+
+    curl -X DELETE $URL/$ID
+fi
+
+exit 0
